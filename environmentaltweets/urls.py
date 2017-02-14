7193478199eefc7d388c5cf507c199a5fc5d3e25
Nick Moolenijzer <nick@moolenijzer.com>
2017-02-13 21:13:40 -0500

    styling

--- environmentaltweets/urls.py
+++ environmentaltweets/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^twitter/', include('twitter.urls')),
+    url(r'^', include('twitter.urls')),
     url(r'^admin/', admin.site.urls),
 ]
-
