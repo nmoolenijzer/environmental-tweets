e13fa8b8febeb3672ccf4e6abc536e1c3126b587
Nick Moolenijzer <nick@moolenijzer.com>
2017-02-20 16:50:58 -0500

    working graph

--- twitter/views.py
+++ twitter/views.py
@@ -9,6 +9,15 @@ from nltk.corpus import names
 from nltk.corpus import pros_cons
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.dates import DateFormatter
+from django.urls import reverse
+from dateutil import parser
+
+dates = []
+sentiments = []
 
 def analyzeJSON(classifier, jsonResponse, items, counter):
 	for obj in jsonResponse['statuses']:
@@ -17,23 +26,49 @@ def analyzeJSON(classifier, jsonResponse, items, counter):
 		pos = nltk.pos_tag(tokens)
 		words_to_analyze = []
 		for (word, tag) in pos:
-			if ("JJ" in tag or "RB" in tag) and (not("@" in tag)):
+			if not("@" in tag):
 				words_to_analyze.append(word)
 		for word in words_to_analyze:
 			classification = classifier.classify({ 'word': word })
 			score = classifier.prob_classify({ 'word': word }).prob(classification)
 			if (classification == "pro"):
-				total += (score*100)
+				if ("JJ" in word or "RB" in word): total += (score*100)
+				else: total += (score*5)
 			else:
-				total -= (score*100)
+				if ("JJ" in word or "RB" in word): total -= (score*100)
+				else: total -= (score*5)
 		avg = 0;
 		if len(words_to_analyze) != 0: avg = total/float(len(words_to_analyze))
 
 		items.append({ 'name': obj['user']['name'], 'text': obj['full_text'], 'avg': avg, 'count': counter })
 
+		datetimeObj = parser.parse(obj['created_at'])
+		dates.append(datetimeObj)
+		sentiments.append(total)
+
 		counter += 1
 
 	return counter
+
+def show_chart(request):
+
+	figure = Figure()
+	plt = figure.add_subplot(111)
+	figure.set_size_inches(12, 7)
+
+	plt.set_title("Sentiment Analysis of Tweets Containing @EPA")
+
+	plt.set_xlabel("Dates")
+	plt.set_ylabel("Sentiment")
+
+	plt.plot_date(dates, sentiments, '-', color="#031634", linewidth=1)
+
+	canvas = FigureCanvas(figure)
+
+	response = HttpResponse(content_type="image/png")
+	canvas.print_png(response)
+
+	return response
 
 def index(request):
 	nltk.data.path.append('./static/twitter/nltk_dir')
@@ -68,7 +103,7 @@ def index(request):
 
 	counter = analyzeJSON(classifier, jsonResponse, items, counter)
 
-	while ('next_results' in jsonResponse['search_metadata'] and counter < 102):
+	while ('next_results' in jsonResponse['search_metadata']): # and counter < 102):
 		resp, content = client.request( 'https://api.twitter.com/1.1/search/tweets.json' + str(jsonResponse['search_metadata']['next_results']) + '&tweet_mode=extended&exclude_replies=true', method="GET", body=b"", headers=None )
 
 		stringResponse = content.decode("utf-8")
@@ -76,10 +111,11 @@ def index(request):
 
 		counter = analyzeJSON(classifier, jsonResponse, items, counter)
 
+	# image = (buffer.getValue(), "image/png")
     # print("\nConfidence can also be analyzed:")
     # print(nltk.classify.accuracy(classifier, tester));
 	#
     # print("\nAnd which training data is most important:")
     # print(classifier.show_most_informative_features(5));
 
-	return HttpResponse(render(request, 'index.html', {'items': items}, content_type='application/html'))
+	return HttpResponse(render(request, 'index.html', {'items': items, 'image': reverse('show_chart')}, content_type='application/html'))
